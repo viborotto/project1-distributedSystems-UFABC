17bf5ddf8cddaa96a1c63d589c958a5dab4d6ae4
viborotto <vittoriaborotto@gmail.com>
2023-06-22 15:57:14 -0300

    Rename socket peerX

--- peers/peerX.py
+++ peers/peerX.py
@@ -1,15 +1,14 @@
 import socket, os
 
-s = socket.socket()
-
+peerXsocket = socket.socket()
 # define the port on which you want connect
 # nesse caso a porta que definimos no servidor
 server_port = 1099
 server_ip = '127.0.0.1'
 
-s.connect((server_ip, server_port))
-address = s.getsockname()
-peer_ip, peer_port = s.getsockname()
+peerXsocket.connect((server_ip, server_port))
+address = peerXsocket.getsockname()
+peer_ip, peer_port = peerXsocket.getsockname()
 diretorio = "directoryX"
 
 def listPeerFiles():
@@ -28,9 +27,9 @@ while True:
         mensagem_join.append("JOIN")
         mensagem_join.append(lista_arquivos)
         ## MANDAR A OPERACAO JOIN E A LISTA PARA O SERVIDOR
-        s.send(str(mensagem_join).encode())
+        peerXsocket.send(str(mensagem_join).encode())
         print("Enviada mensagem com OPERACAO JOIN: "+ str(mensagem_join))
-        if s.recv(1024).decode() == "JOIN_OK":
+        if peerXsocket.recv(1024).decode() == "JOIN_OK":
             print(">>> Sou peer " + peer_ip + ":" + str(peer_port) + " com arquivos " + ", ".join(lista_arquivos))
 
     ## SEARCH OPERATION: busca no servidor em quais peers o arquivo esta disponivel
@@ -40,8 +39,8 @@ while True:
         mensagem_search = []
         mensagem_search.append("SEARCH")
         mensagem_search.append(arquivo)
-        s.send(str(mensagem_search).encode())
-        peers_list = s.recv(2048).decode()
+        peerXsocket.send(str(mensagem_search).encode())
+        peers_list = peerXsocket.recv(2048).decode()
         print(">>> peers com arquivo solicitado: " + peers_list)
 
     elif operacao == '3':
@@ -51,9 +50,9 @@ while True:
         mensagem_download.append("DOWNLOAD")
         ## enviar para o servidor tambem o nome do arquivo que deseja baixar
         mensagem_download.append(arquivo)
-        s.send(str(mensagem_download).encode())
+        peerXsocket.send(str(mensagem_download).encode())
 
-        peers_list_download = s.recv(2048).decode()
+        peers_list_download = peerXsocket.recv(2048).decode()
         print("Peers com arquivo para download: " + peers_list_download)
 
         peer_escolhido = input("Escolha o Peer que deseja realizar o download do arquivo: ")
@@ -63,7 +62,7 @@ while True:
 
         # TODO: PeerX deve requisitar o arquivo para o peer_escolhido, por exemplo PeerY
         # De acordo com o peer escolhido iremos abrir uma conexao com o Peer definido para que haja o download
-        peer_solicitante_sock.bind((peer_escolhido_ip, peer_escolhido_port))
+        peer_solicitante_sock.bind((peer_escolhido_ip, int(peer_escolhido_port)))
         print("peer_solicitante_sock binded to %s" % (peer_escolhido_port))
         peer_solicitante_sock.listen(5)
         print("PeerX socket is listening")
@@ -72,9 +71,9 @@ while True:
 
         ## PeerY podera aceitar ou negar o pedido
         ## como o peerY vai receber que o peerX ta solicitando um arquivo?
-        if s.recv(2048).decode() == "yes":
+        if peerXsocket.recv(2048).decode() == "yes":
             ## todo: fazer download
-            print("yees")
+            print("yees pronto para realizar o download em PeerX")
 
 
 
@@ -85,7 +84,7 @@ while True:
         # if opcao == "yes":
 
     elif operacao == 'quit':
-        s.send(operacao.encode())
+        peerXsocket.send(operacao.encode())
         break
 
-s.close()
+peerXsocket.close()
